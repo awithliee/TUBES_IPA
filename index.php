bba39ceee8404be15b6f343a6897fb8fe2a7a5d7
awithliee <alipasundan24@gmail.com>
2026-01-16 10:01:05 +0700

    tes

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ $message = isset($_GET['message']) ? urldecode($_GET['message']) : '';
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
         <div class="container-fluid">
             <a class="navbar-brand fw-bold" href="index.php">
-                <i class="bi bi-box-seam-fill"></i> Inventaris Barang
+                <i class="bi bi-box-seam-fill"></i> Inventaris Batang
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
